40c5086da9036a1ac356713018423b9285ae014c
achmadrifqi <achmarifqirosadi@webmail.umm.ac.id>
2022-04-01 01:23:20 +0700

    Initial Commit

---
+++ QuranOnline/QuranOnlineApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuranOnlineApp.swift
+//  QuranOnline
+//
+//  Created by Achmad Rifqi on 01/04/22.
+//
+
+import SwiftUI
+
+@main
+struct QuranOnlineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
